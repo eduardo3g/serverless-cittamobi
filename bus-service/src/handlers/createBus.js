49d652f16819b52fc5185ba46004c933e27b6db1
eduardo3g <eduardosbrasil10@gmail.com>
2020-06-21 17:17:50 -0300

    chore: Validating if bus line or bus type exists when creating a bus.

--- bus-service/src/handlers/createBus.js
+++ bus-service/src/handlers/createBus.js
@@ -5,11 +5,25 @@ import validator from '@middy/validator';
 import commomMiddleware from '../lib/commomMiddleware';
 import createBusSchema from '../lib/schemas/createBusSchema';
 
+import { getBusLineById } from './getBusLine';
+import { getBusTypeById } from './getBusType';
+
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createBus(event, context) {
   const { chassis, line_id, bus_type_id } = event.body;
   const now = new Date();
+
+  const busLine = await getBusLineById(line_id);
+  const busType = await getBusTypeById(bus_type_id);
+
+  if (!busLine) {
+    throw new createError.NotFound(`A linha de ônibus com ID ${line_id} não existe.`);
+  }
+
+  if (!busType) {
+    throw new createError.NotFound(`A categoria de ônibus com ID ${bus_type_id} não existe.`);
+  }
 
   const bus = {
     id: uuid(),
